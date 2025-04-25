2b07d64818d9dfceb3779eadbe6a65227477c8bf
G/ <g@moigneu.com>
2025-04-25 10:27:37 -0500

    Update Next.js configuration to use standalone output and adjust TypeScript settings for improved structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
