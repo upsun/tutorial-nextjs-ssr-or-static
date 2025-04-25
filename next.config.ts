8dc90403d6f31f442d2f9831cc303176460d4350
G/ <g@moigneu.com>
2025-04-25 10:02:09 -0500

    Update Next.js configuration and dependencies; add .upsun configuration for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  distDir: 'out',
 };
 
 export default nextConfig;
